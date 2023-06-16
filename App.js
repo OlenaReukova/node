397d249a68b882910c25874505307f446ef34b23
Olena Reukova <kronverk01@gmail.com>
2023-06-16 16:05:30 +0100

    POST request
    create

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const fs = require ('fs');
 const express = require ('express');
 
 const app = express();
+app.use(express.json()); //Middleware
 
 // app.get('/', (req, res) => {
 //     res
@@ -27,6 +28,22 @@ app.get('/api/v1/tours', (req, res)=>{
     });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+  
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/4-natours/after-section-06/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    });
+  });
+  
 const port = 3000;
 app.listen(port, ()=> {
     console.log(`App running on port ${port}...`);
