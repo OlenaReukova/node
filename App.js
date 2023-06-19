720e5ab183d94a76650fbc68905af82b66529535
Olena Reukova <kronverk01@gmail.com>
2023-06-19 22:16:32 +0100

    Middleware from Morgan
    HTTP request logger middleware for node.js

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 const fs = require ('fs');
 const express = require ('express');
+const morgan = require('morgan');
 
 const app = express();
-//Middleware
+
+//1.Middleware
+app.use(morgan('dev'));
 app.use(express.json()); 
 
 //Middleware
@@ -30,6 +33,8 @@ app.use((req, res, next)=>{
 const tours =JSON.parse(
     fs.readFileSync(`${__dirname}/4-natours/after-section-06/dev-data/data/tours-simple.json`)
 );
+
+//2 Route Handlers
 
 const getAllTours = (req, res)=>{
   console.log(req.requestTime);
@@ -123,6 +128,7 @@ const deleteTour=(req, res)=>{
 // app.patch('/api/v1/tours/:id', updateTour );
 // app.delete('/api/v1/tours/:id', deleteTour);
   
+//3. Routes
 app
 .route('/api/v1/tours')
 .get(getAllTours)
@@ -134,6 +140,8 @@ app
 .patch(updateTour)
 .delete(deleteTour);
 
+
+//4. Start Server
 const port = 3000;
 app.listen(port, ()=> {
     console.log(`App running on port ${port}...`);
